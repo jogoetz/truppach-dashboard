f51168d41e2cc0bc2e78f2e50eed3664e6b5e263
jogoetz <joachim.goetz@uni-bayreuth.de>
2026-07-08 16:54:01 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -84,10 +84,9 @@ def load_data():
 # -----------------------------
 
 @st.cache_data(ttl=600)
-def load_hnd_abfluss(today_str):
+def load_hnd_abfluss():
  
     url = "https://www.hnd.bayern.de/pegel/oberer_main_elbe/plankenfels-24244504/tabelle?methode=abfluss&begin=01.01.2025&end={today}&setdiskr=15"
-    url = url.replace("{today}", today_str)
     
     tables = pd.read_html(url, flavor="bs4", decimal=",", thousands=".")
     if not tables:
@@ -113,10 +112,9 @@ def load_hnd_abfluss(today_str):
 # -----------------------------
 
 @st.cache_data(ttl=600)
-def load_behringersmuehle(today_str):
+def load_behringersmuehle():
 
     url = "https://www.gkd.bayern.de/de/fluesse/schwebstoff/regnitz/behringersmuehle-24241710/gesamtzeitraum/tabelle?zr=gesamt&parameter=konzentration&parameterNr=14&beginn=01.01.2025&ende={today}"
-    url = url.replace("{today}", today_str)
     
     try:
         tables = pd.read_html(
