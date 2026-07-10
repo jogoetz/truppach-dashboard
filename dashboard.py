42661bdcf0cff09e6a71109a81e89fcd8fb496df
jogoetz <joachim.goetz@uni-bayreuth.de>
2026-07-10 14:51:12 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -664,7 +664,7 @@ line-height: 1.5;
 m.get_root().html.add_child(folium.Element(legend_html))
 
 # ✅ Anzeige
-map_data = st_folium(m, height=700, use_container_width=True)
+map_data = st_folium(m, height=500, use_container_width=True)
 
 st.write(map_data)
 
